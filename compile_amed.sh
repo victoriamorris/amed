718c8dd694cdee39ee7f0abf3d1218a9a4fdf74f
victoriamorris <victoria.morris@bl.uk>
2024-02-07 19:10:01 +0000

    Add files via upload

--- compile_amed.sh
+++ compile_amed.sh
@@ -1,8 +1,8 @@
-python setup.py install
-pyinstaller amed_tools.py -F
-read -p "Press [Enter]"
-mv dist/amed_tools.exe amed_tools.exe
-rmdir dist
-rm -rf __pycache__
+python -m PyInstaller bin/amed_pre.py -F
+mv dist/amed_pre.exe exe/amed_pre.exe
+python -m PyInstaller bin/amed_post.py -F
+mv dist/amed_post.exe exe/amed_post.exe
+rm -rf amed_tools/__pycache__
 rm -rf build
+rm *.spec
 read -p "Press [Enter]"
--- setup.py
+++ setup.py
@@ -4,47 +4,43 @@
 """setup.py file for amed_tools."""
 
 # Import required modules
-import re
 from distutils.core import setup
 
 __author__ = 'Victoria Morris'
 __license__ = 'MIT License'
-__version__ = '1.0.0'
+__version__ = '3.0.0'
 __status__ = '4 - Beta Development'
-
-# Version
-version = '2.0.0'
-
-# Long description
-long_description = ''
 
 # List requirements.
 # All other requirements should all be contained in the standard library
 requirements = [
-    'pyinstaller',
-	'regex',
-	'csv',
+    'regex',
+    'sqlite3'
 ]
 
 # Setup
 setup(
     console=[
-        'amed/amed_pre.py',
-        'amed/amed_post.py',
+        'bin/amed_pre.py',
+        'bin/amed_post.py',
     ],
     zipfile=None,
-    options=None,
+    options={
+        'py2exe': {
+            'bundle_files': 0,
+        }
+    },
     name='amed',
-    version=version,
+    version='3.0.0',
     author='Victoria Morris',
-    url='',
+    url='https://github.com/victoriamorris/AMED',
     license='MIT',
     description='Scripts for processing AMED files.',
-    long_description=long_description,
-    packages=['amed'],
+    long_description='Scripts for processing AMED files.',
+    packages=['amed_tools',],
     scripts=[
-        'amed/amed_pre.py',
-        'amed/amed_post.py',
+        'bin/amed_pre.py',
+        'bin/amed_post.py',
     ],
     classifiers=[
         'Development Status :: 4 - Beta',
